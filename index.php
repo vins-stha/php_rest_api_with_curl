c8689f331929647d9875972288c2923f8659e2e3
vins-stha <shrs_samar@yahoo.com>
2022-06-16 15:05:37 +0300

    simple error handling

--- index.php
+++ index.php
@@ -19,38 +19,50 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
   $result = $apiCall->createCurlRequest();
 
   $accesstoken = trim($result['access_token']);
+  $array = explode('.', $_FILES['file']['name']);
+  $extension = end($array);
 
-  if (isset($_FILES['file']['tmp_name']) && (filesize($_FILES['file']['tmp_name']) <= ApiCall::MAXUPLOAD) ) {
+  try {
+    if (isset($_FILES['file']['tmp_name'])
+        && (filesize($_FILES['file']['tmp_name']) <= ApiCall::MAXUPLOAD)
+        && (strcmp(strtolower($extension), 'json') == 0)
+        && ($_FILES['file']['error'] === UPLOAD_ERR_OK)
+    ) {
 
+      $cfile = new CURLFile($_FILES['file']['tmp_name'], $_FILES['file']['type'], $_FILES['file']['name']);
+      $data = array("file" => $cfile);
 
-    $cfile = new CURLFile($_FILES['file']['tmp_name'], $_FILES['file']['type'], $_FILES['file']['name']);
-    $data = array("file" => $cfile);
+      $uploadUrl = ApiCall::BASEURL . "mass-sale/create_mass_post_sale_orders";
 
-    $uploadUrl = ApiCall::BASEURL . "mass-sale/create_mass_post_sale_orders";
+      $uploadCurl = new ApiCall($uploadUrl, $method = "POST", $data, $accesstoken);
 
-    $uploadCurl = new ApiCall($uploadUrl, $method = "POST", $data, $accesstoken);
+      $uploadCurl->setCurlToken($accesstoken);
 
-    $uploadCurl->setCurlToken($accesstoken);
+      $result = $uploadCurl->createCURLRequest();
 
-    $result = $uploadCurl->createCURLRequest();
-
-    $curlUrl = ApiCall::BASEURL . "mass-sale/get_mass_post_sale_order_status/".$result['job_id'];
-    $mass_sale_curl = new ApiCall($curlUrl, $method = "GET", null, $_SESSION['access_token']);
-    $result = $mass_sale_curl->createCurlRequest();
-
-    $str = "Cannot return order response list, Mass post sale job: ".$result['job_id']. " is still pending.";
-
-    while (strcmp($result['message'], $str) == 0)
-    {
-      $mass_sale_curl = new ApiCall($curlUrl, $method = "GET", null, $_SESSION['access_token']);
+      $curlUrl = ApiCall::BASEURL . "mass-sale/get_mass_post_sale_order_status/" . $result['job_id'];
+      $mass_sale_curl = new ApiCall($curlUrl, $method = "GET", null, $accesstoken);
       $result = $mass_sale_curl->createCurlRequest();
-    }
-    $_SESSION['access_token'] = $accesstoken;
-    $_SESSION['job_id'] = $result['job_id'];
-    $_SESSION['order_data'] = $result;
 
-    if (!empty($result) && $result['job_id'] !== null)
-      header("Location: /eas/results.php");
+      $str = "Cannot return order response list, Mass post sale job: " . $result['job_id'] . " is still pending.";
+
+      while (strcmp($result['message'], $str) == 0) {
+        $mass_sale_curl = new ApiCall($curlUrl, $method = "GET", null, $accesstoken);
+        $result = $mass_sale_curl->createCurlRequest();
+      }
+      $_SESSION['access_token'] = $accesstoken;
+      $_SESSION['job_id'] = $result['job_id'];
+      $_SESSION['order_data'] = $result;
+
+      if (!empty($result) && $result['job_id'] !== null)
+        header("Location: /eas/results.php");
+
+    }
+    else {
+      throw new RuntimeException('Invalid parameters. File type allowed: json');
+    }
+  } catch (RuntimeException $exception) {
+    echo $exception->getMessage();
   }
 
 }
